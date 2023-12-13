7587062cc03cb8c06a27bc2de233cd200a98c33b
Dell <Dell@DESKTOP-QSA5BIL>
2023-12-13 14:17:13 +0530

    Revert "commit4"
    This reverts commit 10e8b0890233a1e019b61f1fe00807e7c30df940.

--- src/day12_12_2023_github/Test1.java
+++ src/day12_12_2023_github/Test1.java
@@ -2,7 +2,7 @@ package day12_12_2023_github;
 
 public class Test1 {
 
-	int price=123;
+	int price;
         //remote repository changes
 	float weight;
 public static void main(String [] args){
@@ -10,9 +10,6 @@ public static void main(String [] args){
 	System.out.println("Test 1..");
 	System.out.println("Test 2..");
 	System.out.println("Test 3..");
-	
-	Test1 t=new Test1();
-	System.out.println("Creating object ");
 
 }
 }
