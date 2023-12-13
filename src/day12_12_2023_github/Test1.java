10e8b0890233a1e019b61f1fe00807e7c30df940
Dell <Dell@DESKTOP-QSA5BIL>
2023-12-13 14:14:22 +0530

    commit4

--- src/day12_12_2023_github/Test1.java
+++ src/day12_12_2023_github/Test1.java
@@ -2,7 +2,7 @@ package day12_12_2023_github;
 
 public class Test1 {
 
-	int price;
+	int price=123;
         //remote repository changes
 	float weight;
 public static void main(String [] args){
@@ -10,6 +10,9 @@ public static void main(String [] args){
 	System.out.println("Test 1..");
 	System.out.println("Test 2..");
 	System.out.println("Test 3..");
+	
+	Test1 t=new Test1();
+	System.out.println("Creating object ");
 
 }
 }
